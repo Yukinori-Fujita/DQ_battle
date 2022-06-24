48b2f52f9358c65c6626feaa0777595a5285f819
藤田 幸法 <straight.0429@gmail.com>
2022-06-25 00:20:38 +0900

    ちょっとずつ骨組みを完成させています。

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// $(function () {
+//     $.ajax({
+//         url: 'https://pokeapi.co/api/v2/pokemon/pikachu/', //アクセスするURL
+//         type: 'get', //post or get
+//         cache: false,        //cacheを使うか使わないかを設定
+//         dataType: 'json',     //data type script・xmlDocument・jsonなど
+//     })
+//         .done(function (response) {
+//             console.log(response.sprites.front_default);
+//             //通信成功時の処理
+//             //成功したとき実行したいスクリプトを記載
+//         })
+//         .fail(function (xhr) {
+//             //通信失敗時の処理
+//             //失敗したときに実行したいスクリプトを記載
+//         })
+//         .always(function (xhr, msg) {
+//             //通信完了時の処理
+//             //結果に関わらず実行したいスクリプトを記載
+//         });
+// })
